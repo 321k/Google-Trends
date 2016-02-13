3902b76c04f79f746f740b41ca5e0f75e215b9b0
Erik Johansson <erik.johansson@transferwise.com>
2016-02-13 14:58:43 +0000

    Create create_gt_url.js
    The URL_GT implemented in javascript.

---
+++ create_gt_url.js
@@ -0,0 +1,40 @@
+'use strict';
+
+
+function URL_GT(keyword, country, region, year, month, length){
+  
+  var start = "http://www.google.com/trends/trendsReport?hl=en-US&q=";
+  var end = "&cmpt=q&content=1&export=1";
+  var geo = "";
+  var date = "";
+  var URL = "";
+
+  
+  //Geographic restrictions
+  if(typeof country!=="undefined") {
+    geo="&geo=";
+    geo=geo + country;
+    if(region!==undefined) geo=geo + "-" + region;
+  }
+  
+  if(typeof keyword==="string"){
+  	var queries=keyword;
+  }
+  
+  if(typeof keyword==="object"){
+  	var queries=keyword[0];
+    for(var i=1; i < keyword.length; i++){
+      queries=queries + "%2C" + keyword[i];
+    }
+  }
+  
+  //Dates
+  if(typeof year!=="undefined"){
+    date="&date="
+    date=date + month + "%2F" + year + "%20" + length + "m"
+  }
+  
+  URL = start + queries + geo + date + end;
+  //URL = URL.replace(" ", "%20");
+  return(URL);
+}
